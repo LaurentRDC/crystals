6e9a402cfa8f3c93deaddcb5920eb8965ff87080
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2018-11-20 12:53:59 -0500

    Version bump to 0.5.1

--- crystals/__init__.py
+++ crystals/__init__.py
@@ -5,7 +5,7 @@ This package allows for manipulation and modelling of atomic structures in cryst
 __author__ = "Laurent P. René de Cotret"
 __email__ = "laurent.renedecotret@mail.mcgill.ca"
 __license__ = "BSD3"
-__version__ = "0.5"
+__version__ = "0.5.1"
 
 from .atom import Atom
 from .atom import Element
