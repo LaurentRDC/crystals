ecad2cb3075e8e5c6820b5e34bf5277870266fc6
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2019-12-10 16:20:55 -0500

    [skip ci] Changed to markdown readme

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(base_path / PACKAGE_NAME / "__init__.py") as f:
         re.compile(r".*__license__ = \"(.*?)\"", re.S).match(module_content).group(1)
     )
 
-with open("README.rst") as f:
+with open("README.md") as f:
     README = f.read()
 
 with open("requirements.txt") as f:
