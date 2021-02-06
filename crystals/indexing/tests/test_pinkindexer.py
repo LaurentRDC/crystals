8a85f117523d0a43b5a01f5250b5561df656665f
Laurent P. René de Cotret <laurent.decotret@outlook.com>
2021-02-06 15:17:37 -0500

    Fixed test

--- crystals/indexing/tests/test_pinkindexer.py
+++ crystals/indexing/tests/test_pinkindexer.py
@@ -8,27 +8,21 @@ from pathlib import Path
 DATADIR = Path(__file__).parent / "data"
 
 
-def test_index_pink_trivial():
-    """ Test a bogus indexing to see if things don't crash. """
-    intensities = np.array([0])
-    peaks = np.array([[0, 0]])
-    r = index_pink(
-        peaks, intensities, 1, 1, 1, 1, detector_radius=1, initial=Lattice(np.eye(3))
-    )
-    assert type(r) == Lattice
-
-
 def test_index_pink_desy_data():
     """ Test index_pink with data provided by DESY's Yaroslav Gevorkov """
     intensities = np.loadtxt(DATADIR / "intensities_new")
     peaks = np.loadtxt(DATADIR / "peaksOnDetector_m_new").T
+    peaks = np.array(peaks[:, ::-1])
+
+    assert peaks.shape[0] == intensities.shape[0]
+    assert peaks.shape[1] == 2
 
     basis = np.diag([0.0126422250316056, 0.0126422250316056, 0.0263157894736842])
 
     indexed, num_indexed = _pinkindexer.index_pink(
         peaks=peaks,
         intensities=intensities,
-        detector_distance=(0.2500),
+        detector_distance=float(0.2500),
         beam_energy=float(8.0010e03),
         divergence_angle=float(0.1 * np.pi / 180),
         non_monochromaticity=float(0.25),
