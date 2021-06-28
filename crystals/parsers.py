Fix scaling factor problem

crystals/parsers.py
@@ -1133,8 +1133,9 @@ class POSCARParser(AbstractStructureParser):
 
             next(f)
         
-            self._sym_ops = [np.eye(3) * float(next(f))]
-            self._lattice_vectors = np.array([next(f).split() for _ in range(3)]).astype(np.float)
+            scaling_factor = float(next(f))
+
+            self._lattice_vectors = np.array([next(f).split() for _ in range(3)]).astype(np.float64) * scaling_factor
             self._atom_types = list(zip(
                 next(f).strip().split(),
                 map(int, next(f).strip().split()),
@@ -1147,7 +1148,8 @@ class POSCARParser(AbstractStructureParser):
             elif flag[0] in ["C", "c", "K", "k"]:
                 for element, nat in self._atom_types:
                     for _ in range(nat):
-                        coords = np.array(next(f).strip().split()).astype(np.float)
+                        coords = np.array(next(f).strip().split()[:3]).astype(np.float64)
+                        coords *= scaling_factor
                         coords = coords @ np.linalg.inv(self._lattice_vectors)
                         self._atoms.append(
                             Atom(element, coords)
@@ -1155,7 +1157,7 @@ class POSCARParser(AbstractStructureParser):
             else:
                 for element, nat in self._atom_types:
                     for _ in range(nat):
-                        coords = np.array(next(f).strip().split()).astype(np.float)
+                        coords = np.array(next(f).strip().split()[:3]).astype(np.float64)
                         self._atoms.append(
                             Atom(element, coords)
                         )